b94b344385022aaf3ede9bd63368704fadcb5773
Hapikc <136709525532hs@gmail.com>
2024-12-09 18:37:20 +0700

    alomost done 2.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-class AdvUser(AbstractUser):
-    is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
 
-    class Meta(AbstractUser.Meta):
-        pass
+
+class AdvUser(AbstractUser):
+   is_activated = models.BooleanField(default=True, db_index=True,verbose_name='Прошел активацию?')
+   fio = models.CharField(max_length=100, blank=True, verbose_name='ФИО')
+
+   class Meta(AbstractUser.Meta):
+       pass
--- catalog/urls.py
+++ catalog/urls.py
@@ -3,9 +3,10 @@ from . import views
 from .views import BBLoginView
 from .views import profile
 from .views import BBLogoutView
+from .views import RegisterUserView, RegisterDoneView
 
 
-
+app_name = 'catalog'
 
 
 urlpatterns = [
@@ -13,4 +14,8 @@ urlpatterns = [
     path('accounts/login', BBLoginView.as_view(), name='login'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+
+
 ]
